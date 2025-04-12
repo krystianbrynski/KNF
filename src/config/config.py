5c8d2d131a806222d23c5a7d5e15880631e4c11d
Krystian Bryński <krystian.brynski@student.put.poznan.pl>
2025-04-12 23:55:47 +0200

    Added config

---
+++ src/config/config.py
@@ -0,0 +1,9 @@
+FILE_LAB_CODES = r"D:\Pycharm\Pierwszyprogram\KNF1\data\taxonomy\TaksonomiaBION\TaksonomiaBION\www.uknf.gov.pl\pl\fr\xbrl\fws\bion\bion-2024-12\2024-10-21\tab\n.rr.bk.01.00\n.rr.bk.01.00-lab-codes.xml"
+FILE_LAB_PL = r"D:\Pycharm\Pierwszyprogram\KNF1\data\taxonomy\TaksonomiaBION\TaksonomiaBION\www.uknf.gov.pl\pl\fr\xbrl\fws\bion\bion-2024-12\2024-10-21\tab\n.rr.bk.01.00\n.rr.bk.01.00-lab-pl.xml"
+
+NS = {
+    "link": "http://www.xbrl.org/2003/linkbase",
+    "xlink": "http://www.w3.org/1999/xlink",
+    "label": "http://xbrl.org/2008/label",
+    "gen": "http://xbrl.org/2008/generic"
+}
--- src/main.py
+++ src/main.py
@@ -1,16 +1,11 @@
 from parser.parse_file import parse_label_file
 from src.extractor.extract_two_files import extract_from_pl_codes
 from src.extractor.extract_tab_col import extract_tab_col
+from config import config
 
-FILE_LAB_CODES = r"D:\Pycharm\Pierwszyprogram\KNF1\data\taxonomy\TaksonomiaBION\TaksonomiaBION\www.uknf.gov.pl\pl\fr\xbrl\fws\bion\bion-2024-12\2024-10-21\tab\n.rr.bk.01.00\n.rr.bk.01.00-lab-codes.xml"
-FILE_LAB_PL = r"D:\Pycharm\Pierwszyprogram\KNF1\data\taxonomy\TaksonomiaBION\TaksonomiaBION\www.uknf.gov.pl\pl\fr\xbrl\fws\bion\bion-2024-12\2024-10-21\tab\n.rr.bk.01.00\n.rr.bk.01.00-lab-pl.xml"
-
-NS = {
-    "link": "http://www.xbrl.org/2003/linkbase",
-    "xlink": "http://www.w3.org/1999/xlink",
-    "label": "http://xbrl.org/2008/label",
-    "gen": "http://xbrl.org/2008/generic"
-}
+FILE_LAB_CODES = config.FILE_LAB_CODES
+FILE_LAB_PL = config.FILE_LAB_PL
+NS = config.NS
 
 
 def run_pipeline() -> None:
