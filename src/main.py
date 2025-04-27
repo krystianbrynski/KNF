0bd9615c8380a5a9beca85fbf86fd6ea96e15eec
Marta <martajjoo9@gmail.com>
2025-04-27 18:36:25 +0200

    Extract types

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
-print("hello")
+import xml.etree.ElementTree as ET
+path_file = "C:/Users/marta/PycharmProjects/KNF/data/taxonomy/TaksonomiaBION/www.uknf.gov.pl/pl/fr/xbrl/dict/met/met.xsd"
+
+tree = ET.parse(path_file)
+root = tree.getroot()
+
+wyniki = []
+
+for elem in root.iter():
+    if elem.tag.endswith('element'):
+        name = elem.attrib.get('name')
+        typ = elem.attrib.get('type')
+        if name and typ:
+            wyniki.append((name, typ))
+
+for name, typ in wyniki:
+    print(f"Nazwa: {name}, Typ: {typ}")
