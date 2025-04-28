a56c695e330c751acf593762582cd69b94ca3218
Marta <martajjoo9@gmail.com>
2025-04-28 14:13:53 +0200

    Cleaned code

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello")
+from create_db.connection import connect
+from config.paths import FILE_PATH
+
+
+def run_pipeline() -> None:
+    connect(FILE_PATH)
+
+
+if __name__ == "__main__":
+    run_pipeline()
