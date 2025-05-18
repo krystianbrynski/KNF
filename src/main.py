3c736ddbb9dd788774f763501ee807044ad9b01d
Krystian Bryński <krystian.brynski@student.put.poznan.pl>
2025-05-18 13:33:53 +0200

    Small change in main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,19 @@
 import json
+import os
 
+from pathlib import Path
 from parser.xml import parse_xml
-from find_paths.find_paths import search_paths
-from find_paths.find_paths import aim_paths
 from extractor.lab_codes import extract_lab_codes_file
 from extractor.rend import extract_rend_file
 from extractor.rend import extract_rend_file_labels_and_qnames
 from extractor.lab_pl import extract_lab_pl_file
 from extractor.namespaces import extract_namespaces
 from extractor.data_types import extract_types
-from extractor.extract_id import extract_report_id
 from merge.merge_data import prepare_data_to_load
 from merge.merge_data import prepare_label_and_data_type
 from merge.merge_data import prepare_type_into_dict
 from transformator.transform_data import transform_labels_unique
-import os
-from pathlib import Path
+
 
 def extract_prefix(name: str) -> str:
     parts = name.split(".")
@@ -33,35 +31,22 @@ def collect_unique_folder_prefixes(root_dir: str):
 
     return sorted(unique_names)
 
-def run_pipeline() -> None:
-    target_folders = extract_report_id()
-    paths = search_paths(target_folders)
-    lab_pl_rend_lab_codes_paths = aim_paths(paths)
+def create_structure() -> None:
     met = parse_xml(r'..\data\taxonomy\TaksonomiaBION\www.uknf.gov.pl\pl\fr\xbrl\dict\met\met.xsd')
-    combined_data = []
     folder_path = "../data/taxonomy/TaksonomiaBION/www.uknf.gov.pl/pl/fr/xbrl/fws/bion/bion-2024-12/2024-10-21/tab"
     prefixes = collect_unique_folder_prefixes(folder_path)
 
     for i, prefix in enumerate(prefixes):
-
-        #kod do dodania nazw formularza + id_formualrza
-        # Przechodzimy po wszystkich elementach w folderze
         for folder_name in os.listdir(folder_path):
             folder_path_full = os.path.join(folder_path, folder_name)
 
-
-            # Sprawdzamy, czy to jest folder i zaczyna się od danego prefiksu
             if os.path.isdir(folder_path_full) and folder_name.startswith(prefix):
                 files_of_interest = []
-                # Teraz wypisujemy wszystkie pliki w tym folderze
+
                 for file_name in os.listdir(folder_path_full):
-                    file_path = os.path.join(folder_path_full, file_name)
                     file_name_clean = file_name.strip()
                     file_path = os.path.join(folder_path_full, file_name_clean)
 
-                    # Zamieniamy "\\" na "/" w ścieżce
-                    file_path = file_path.replace("\\", "/")  # Zamiana separatorów
-                    # Sprawdzamy, czy to plik
                     if os.path.isfile(file_path):
                         if 'lab-pl' in file_name or 'rend' in file_name or 'lab-codes' in file_name:
                             files_of_interest.append(file_path)
@@ -78,24 +63,19 @@ def run_pipeline() -> None:
 
                 data, column_flag = prepare_data_to_load(rend_labels, lab_codes_labels_and_value,
                                                                  lab_pl_labels_and_value)
+
                 label_and_data_type = prepare_label_and_data_type(rend_labels_and_qnames, data_types)
                 data_with_types = prepare_type_into_dict(label_and_data_type, data)
                 transformed_data = transform_labels_unique(data_with_types, column_flag, folder_name)
                 folder_name = Path(files_of_interest[0]).parent.name
 
                 wrapped_data = {f"{prefix}": transformed_data}
+
                 with open(f'../data/json/{folder_name}.json', 'a', encoding='utf-8') as json_file:
                    json.dump(wrapped_data, json_file, ensure_ascii=False, indent=4)
 
-
-
-
-
-
-
-
-
-
+def run_pipeline() -> None:
+    create_structure()
 
 
 
